9b1203ae2a8195733cc70451aeba90879140d8d1
Emilien Klein <emilien@klein.st>
2019-04-04 22:48:43 +0200

    Add copyright and license notices to all the source files

--- bulkscanbarcodesgen.js
+++ bulkscanbarcodesgen.js
@@ -1,6 +1,25 @@
 /*jslint browser: true, white: true, long: true */
 /*global DrawHTMLBarcode_Code128B, Departments, DocumentTypes */
 
+/*
+Copyright 2019 Emilien Klein
+
+This file is part of bulkscanbarcodesgen.
+
+bulkscanbarcodesgen is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+bulkscanbarcodesgen is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with bulkscanbarcodesgen.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Shorthand for document.getElementById
 var $ = function(id) { return document.getElementById(id); };
 
