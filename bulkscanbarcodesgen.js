919358fed570a602b217bfd7d97333b2ec3bc827
Emilien Klein <emilien@klein.st>
2019-04-04 22:33:21 +0200

    Initial files

---
+++ bulkscanbarcodesgen.js
@@ -0,0 +1,121 @@
+/*jslint browser: true, white: true, long: true */
+/*global DrawHTMLBarcode_Code128B, Departments, DocumentTypes */
+
+// Shorthand for document.getElementById
+var $ = function(id) { return document.getElementById(id); };
+
+// Inspired from https://stackoverflow.com/a/11582513/185053 , modified for JSLint
+function getURLParameter(name) {
+  "use strict";
+  // This function will return null if this specific parameter is not found
+  var parameterValue = null;
+  // Perform a regex match to find the value of the parameter from the query string
+  var parameterRegex = new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.search);
+  if (parameterRegex) {
+    // If the regex found a match, replace any occurrence of + by %20
+    parameterValue = parameterRegex[1].replace(/\+/g, "%20");
+    // and perform proper decoding of the URI
+    parameterValue = decodeURIComponent(parameterValue);
+  }
+  // Return either null or the value of that parameter
+  return parameterValue;
+}
+
+// Use the lists from data.js to populate the dropdowns
+function populateSelects(list, selectElement){
+  "use strict";
+  var select = $(selectElement);
+  list.forEach(function (opt) {
+    var el = document.createElement("option");
+    el.textContent = opt.name;
+    el.value = opt.id;
+    select.appendChild(el);
+  });
+}
+
+// Draw a single barcode
+function drawBarcode(elementID, preText) {
+  "use strict";
+  var value = $(elementID).value;
+  var bc = DrawHTMLBarcode_Code128B(value,      // data
+                                    "yes",      // humanReadable
+                                    "in",       // units
+                                    0,          // minBarWidth
+                                    2.5,        // width
+                                    1,          // height
+                                    "bottom",   // textLocation
+                                    "center",   // textAlignment
+                                    "",         // textStyle
+                                    "black",    // foreColor
+                                    "white"     // backColor
+  );
+  $(elementID + "_barcode").innerHTML = preText + bc;
+}
+
+// Draw the Patient and Visit ID barcodes
+function drawStaticBarcodes() {
+  "use strict";
+  drawBarcode("patientID", "Patient: ");
+  drawBarcode("visitID", "Visit: ");
+}
+
+// Draw the Department and Document Type barcodes
+function drawDynamicBarcodes() {
+  "use strict";
+  drawBarcode("departmentID", "Department: ");
+  drawBarcode("documentType", "Document Type: ");
+}
+
+// Validate the parameters passed by URL and populate the page accordingly
+function processParameters(){
+  "use strict";
+  var patientID = getURLParameter("patientID");
+  var visitID = getURLParameter("visitID");
+
+  // Check validity of the patient ID
+  //TODO: use Regex to validate the expected format of the patient ID
+  if(!patientID){
+    $("parameters").style.display = "none";
+    $("barcodes").innerHTML = "NO VALID PATIENT ID PASSED";
+    return false;
+  }
+
+  // Hide the Visit ID if not provided
+  if(!visitID){
+    $("visitID_barcode").style.display = "none";
+  }
+
+  // Set the values passed by the parameters
+  $("patientID").value = patientID;
+  $("visitID").value = visitID;
+  return true;
+}
+
+// Prints the page (unneeded elements are hidden using pring.css)
+function printBarcodes(){
+  window.print();
+}
+
+// The main function
+function main(){
+  // Populate the dropdowns
+  populateSelects(Departments, "departmentID");
+  populateSelects(DocumentTypes, "documentType");
+
+  // Create event listeners (changing dropdown values or print request)
+  $("departmentID").addEventListener("change", drawDynamicBarcodes, false);
+  $("documentType").addEventListener("change", drawDynamicBarcodes, false);
+  $("printButton").addEventListener("click", printBarcodes, false);
+
+  // Read patient and visit IDs from parameters passed to the page
+  if(true === processParameters()){
+    // Only draw the initial barcodes if a valid patient ID was passed
+    drawStaticBarcodes();
+    drawDynamicBarcodes();
+
+    // Give the focus to the departmentID dropdown
+    $("departmentID").focus();
+  }
+}
+
+main();
---
+++ data.js
@@ -0,0 +1,24 @@
+/*jslint browser: true, white: true, long: true */
+
+// Edit these lists to modify the list of Departments and Document Types
+var Departments = [
+  {id: "DeptA", name: "Department A"},
+  {id: "DeptB", name: "Department B"},
+  {id: "DeptC", name: "Department C"},
+  {id: "DeptD", name: "Department D"},
+  {id: "DeptE", name: "Department E"},
+  {id: "DeptF", name: "Department F"},
+  {id: "DeptG", name: "Department G"},
+  {id: "DeptH", name: "Department H"}
+];
+
+var DocumentTypes = [
+  {id: "DocS", name: "Document S"},
+  {id: "DocT", name: "Document T"},
+  {id: "DocU", name: "Document U"},
+  {id: "DocV", name: "Document C"},
+  {id: "DocW", name: "Document W"},
+  {id: "DocX", name: "Document X"},
+  {id: "DocY", name: "Document Y"},
+  {id: "DocZ", name: "Document Z"}
+];
